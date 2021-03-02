eb9ee5d6e0fb98e9b6b0928fb01203784f7c25eb
rytisp91 <68050841+rytisp91@users.noreply.github.com>
2021-03-02 17:13:37 +0200

    added correct route to login method

--- public/index.php
+++ public/index.php
@@ -26,6 +26,9 @@ $app->router->get('/comments', [SiteController::class, 'comments']);
 $app->router->get('/register', [AuthController::class, 'register']);
 $app->router->post('/register', [AuthController::class, 'register']);
 
-$app->router->get('/login', [SiteController::class, 'login']);
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
+
+
 
 $app->run();
