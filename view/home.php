7ccb4d77569e5aefc921ee3092ebc8d96ed55e8e
rytisp91 <68050841+rytisp91@users.noreply.github.com>
2021-03-02 11:39:12 +0200

    Google map added

--- view/home.php
+++ view/home.php
@@ -25,8 +25,11 @@
             </div>
         </div>
     </div>
-
 </div>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.2196022783774!2d25.335696615786055!3d54.723351980290666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96e7d814e149%3A0xdd7887e198efd4c7!2sSaul%C4%97tekio%20al.%2015%2C%20Vilnius%2010224!5e0!3m2!1slt!2slt!4v1614677837372!5m2!1slt!2slt" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+
 
 <script>
     const home = document.getElementById('home')
